0186c96299358dace2647f1d008c1cc608e3c94a
Niklas Sombert <niklas@ytvwld.de>
2014-06-21 11:01:13 +0200

    Added reveal.js.

--- html.py
+++ html.py
@@ -24,28 +24,90 @@ def getHtml(pres, js, prepend=False, append=False):
 	with open(path.join(folder, "index.yaml")) as pres_file:
 		pres_yaml = pres_file.read()
 	pres_yaml = load(pres_yaml)
-
 	markdown = Markdown(extensions=["extra", "codehilite", "wikilinks"])
-
-	# much to do; now only parsing the slides and outputting pure html
-
 	html = ""
-	html += "<!doctype html>\n<html>\n"
-	# TODO: HEAD
-	html += "\t<body>\n"
-	# TODO: divs and ids
-	html += "\t\t<div class=\"reveal\">\n"
-	html += "\t\t\t<div class=\"slides\">\n"
+	html += """
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<!--<title>reveal.js - The HTML Presentation Framework</title>-->
+
+		<!--<meta name="description" content="A framework for easily creating beautiful presentations using HTML">
+		<meta name="author" content="Hakim El Hattab">-->
+
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="reveal.js/css/reveal.css">
+		<link rel="stylesheet" href="reveal.js/css/theme/default.css" id="theme">
+
+		<!-- For syntax highlighting: TODO -->
+
+		<!-- If the query includes 'print-pdf', include the PDF print sheet -->
+		<script>
+			if( window.location.search.match( /print-pdf/gi ) ) {
+				var link = document.createElement( 'link' );
+				link.rel = 'stylesheet';
+				link.type = 'text/css';
+				link.href = 'css/print/pdf.css';
+				document.getElementsByTagName( 'head' )[0].appendChild( link );
+			}
+		</script>
+
+		<!--[if lt IE 9]>
+		<script src="lib/js/html5shiv.js"></script>
+		<![endif]-->
+		
+		<script src="reveal.js/lib/js/head.min.js"></script>
+		<script src="reveal.js/js/reveal.js"></script>
+	</head>
+	
+	<body>
+		<div class="reveal">
+			<div class="slides">
+"""
 	if prepend:
 		html += parse_slide(prepend, 4, markdown, folder)
 	html += parse_slide(pres_yaml, 4, markdown, folder)
 	if append:
 		html += parse_slide(append, 4, markdown, folder)
-	# TODO: divs and ids
-	html += "\t\t\t</div>\n"
-	html += "\t\t</div>\n"
-	html += "\t</body>\n"
-	html += "</html>\n"
+	html += """
+			</div>
+		</div>
+		<!-- TODO: Pull the configuration below from the YAML. -->
+		<script>
+			// Full list of configuration options available here:
+			// https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				controls: true,
+				progress: true,
+				history: true,
+				center: true,
+
+				theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+				transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+
+				// Parallax scrolling
+				// parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+				// parallaxBackgroundSize: '2100px 900px',
+
+				// Optional libraries used to extend on reveal.js
+				dependencies: [
+					{ src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+					{ src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+					{ src: 'reveal.jsplugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+				]
+			});
+
+		</script>
+
+	</body>
+</html>
+"""
 	return(html)
 
 def parse_slide(slide, tabs, markdown, folder):
