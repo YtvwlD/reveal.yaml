6c8e9785ec86b011a4eedfed17148f21f7d7d1ca
Niklas Sombert <niklas@ytvwld.de>
2015-04-05 16:24:44 +0200

    Licensing.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # reveal.yaml - YAML-based presentations with reveal.js
-# Copyright (C) 2014 Niklas Sombert
+# Copyright (C) 2014-2015 Niklas Sombert
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- server.py
+++ server.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
+# reveal.yaml - YAML-based presentations with reveal.js
+# Copyright (C) 2014-2015 Niklas Sombert
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from os import path
 from werkzeug.serving import run_simple
 from werkzeug.wsgi import SharedDataMiddleware
+
 import main
 
 app = SharedDataMiddleware(main.app,
