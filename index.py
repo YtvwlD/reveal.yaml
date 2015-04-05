49bcf86b183b6c7d490e7d98da4a6f94f9a5a4ab
Niklas Sombert <niklas@ytvwld.de>
2015-04-05 16:21:36 +0200

    Reordered the various apps.
    * Use the index.py for CGI.
    * Use the app in server.py for WSGI.
    * The main.py is the main file.

--- index.py
+++ index.py
@@ -1,64 +1,7 @@
 #!/usr/bin/env python
 
-# reveal.yaml - YAML-based presentations with reveal.js
-# Copyright (C) 2014 Niklas Sombert
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from wsgiref.handlers import CGIHandler
-from werkzeug.wrappers import Request, Response
-
-from html import getHtml
-from zip import getZip
-
-try:
-	from sentry import Client
-	client = Client()
-except ImportError:
-	client = None
-
-@Request.application
-def app(request):
-	pres = request.args.get("p")
-	if pres is None:
-		pres = "welcome"
-	
-	get = request.args.get("get")
-	if get is None:
-		get = "html"
-		
-	url = request.url.replace("/index.py/?", "/?")
-
-	try:
-		if get == "zip":
-			zipfile = getZip(pres, (get!="nojs"), url=url)
-			Response(zipfile, mimetype="application/zip")
-		else: # assume get == "html"
-			html = getHtml(pres, (get!="nojs"), url=url)
-			response = Response(html, mimetype="text/html")
-	except:
-		raise
-		#try:
-		#	client.captureException()
-		#	add_to_err = "This incident has been logged."
-		#except AttributeError:
-		#	add_to_err = "This incident hasn't been logged, because logging isn't configured."
-		#except:
-		#	add_to_err = "This incident hasn't been logged, because a error occured while logging the previous error."
-		#html = getHtml("error", (get!="nojs"), append={"text": add_to_err})
-		#response = Response(html, mimetype="text/html")
-	return (response)
+from main import app
 
 if __name__ == "__main__":
 	CGIHandler().run(app)
--- server.py
+++ server.py
@@ -2,12 +2,12 @@
 from os import path
 from werkzeug.serving import run_simple
 from werkzeug.wsgi import SharedDataMiddleware
-from index import app
+import main
 
-middlewareApp = SharedDataMiddleware(app,
+app = SharedDataMiddleware(main.app,
 	{
 	"/reveal.js":	path.join(path.dirname(__file__), "reveal.js"),
 	"/data":		path.join(path.dirname(__file__), "data")
 	})
 if __name__ == "__main__":
-	run_simple("localhost", 4000, middlewareApp)
+	run_simple("localhost", 4000, app)
