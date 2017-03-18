143b6f7481ac7d3b4a5e1e46a9bc669e8e05c819
Niklas Sombert <niklas@ytvwld.de>
2017-03-18 10:46:36 +0100

    It is 2017.

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 # reveal.yaml - YAML-based presentations with reveal.js
-# Copyright (C) 2014-2015 Niklas Sombert
+# Copyright (C) 2014-2017 Niklas Sombert
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
